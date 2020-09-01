65f62837277470c091ac8d5477109ae9c8bce598
Ofek Ashery <ofek.ashery@gmail.com>
2020-09-01 19:07:38 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# COVID-19 Clearance Automation for Mashov
+
+Automation for filling the COVID-19 Clearance on Mashov every day.
+
+## Getting Started
+
+Fill in your login credentials for Mashov and your [school id (סמל מוסד)](https://apps.education.gov.il/imsnet/itur.aspx) on `index.js`.
+
+Run `npm install` and `npm start`.
---
+++ index.js
@@ -0,0 +1,95 @@
+const fetch = require('node-fetch');
+const schedule = require('node-schedule');
+
+////////////////////////////////////////////
+
+const config = {
+  parentId: 000000000,
+  password: '12345678',
+  schoolId: 000000,
+  everyDay: true
+};
+
+////////////////////////////////////////////
+
+const baseUrl = 'https://web.mashov.info/api';
+const apiVersion = '3.20200528';
+
+const log = (...arg) => console.log(`[${new Date().toISOString().split('.')[0]}]`, ...arg);
+
+const login = async () => {
+  log('Logging in to your Mashov account...');
+  return fetch(`${baseUrl}/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8'
+    },
+    body: JSON.stringify({
+      username: String(config.parentId),
+      password: config.password,
+      semel: config.schoolId,
+      year: 2021,
+      apiVersion: apiVersion,
+      appBuild: apiVersion,
+      appVersion: apiVersion,
+      appName: 'הצהרת קורונה אוטומטית'
+    })
+  }).then((res) => {
+    if (!res.ok) return;
+    return res.json().then((json) => ({
+      ...json,
+      csrfToken: res.headers.get('x-csrf-token'),
+      sessionId: res.headers.get('set-cookie').split('MashovSessionID=')[1].split(';')[0]
+    }));
+  });
+};
+
+const sendClearances = async () => {
+  const loginData = await login();
+  if (!loginData) return log('Login failed.');
+  log(`Hello, ${loginData.accessToken.displayName}!`);
+
+  const date = new Date().toISOString().split('.')[0];
+  for (const child of loginData.accessToken.children) {
+    fetch(`${baseUrl}/students/${child.childGuid}/covid/${date}Z/clearance`, {
+      method: 'PUT',
+      headers: {
+        'content-type': 'application/json',
+        'x-csrf-token': loginData.csrfToken,
+        Cookie: `MashovSessionID=${loginData.sessionId}; Csrf-Token=${loginData.csrfToken}`
+      },
+      body: JSON.stringify({
+        answer: 3,
+        answererName: loginData.accessToken.displayName,
+        answererId: loginData.credential.userId,
+        clearanceDate: date,
+        lastAnswer: date,
+        lastAnswerIsOk: true,
+        studentName: child.privateName + child.familyName,
+        studentClass: child.classCode + child.classNum,
+        userId: child.childGuid,
+        noContactWithInfected: true,
+        noHeatAndSymptoms: true
+      })
+    }).then((res) => {
+      if (res.ok) {
+        log(`The clearance was successfully signed for ${child.privateName}`);
+      } else {
+        log(`Signature the clearance for ${child.privateName} failed.`);
+      }
+    });
+  }
+};
+
+if (config.everyDay) {
+  login().then((loginData) => {
+    if (!loginData) return log('Login failed.');
+
+    log('Running. Waiting for 07:00.');
+    schedule.scheduleJob({ hour: 7, minute: 0 }, () => {
+      sendClearances();
+    });
+  });
+} else {
+  sendClearances();
+}
